376fe1389f49ecce5021d66fc78bb97f595c6bdd
jyothi kondupally <131485519+JYOTHIKONDUPALLY@users.noreply.github.com>
2023-11-10 12:56:28 +0000

    change the version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
+ReactDOM.render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById("root"),
 );
